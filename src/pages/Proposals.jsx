163f87b801768a80da01d3a743590a6896cd8810
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-06-19 03:05:11 +0100

    added Proposal Widgets

--- src/pages/Proposals.jsx
+++ src/pages/Proposals.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import Card from "../UI/Card";
+
+import "./Proposals.css";
+
 const Proposals = () => {
-	return <div>Proposals</div>;
+	return (
+		<div className="proposals">
+			<div className="widgets">
+				<Card className="widget">
+					<h3>Total Proposal</h3>
+					<h1>23</h1>
+				</Card>
+				<Card className="widget">
+					<h3>Total Proposal</h3>
+					<h1>23</h1>
+				</Card>
+				<Card className="widget">
+					<h3>Total Proposal</h3>
+					<h1>23</h1>
+				</Card>
+			</div>
+		</div>
+	);
 };
 
 export default Proposals;
